4379b830b6d3b6061995963dcbc1e9875e948638
ljyockey <linyockey@gmail.com>
2017-08-10 21:53:39 -0700

    dashboard.html test

--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -11,6 +12,22 @@ describe('index.html', function() {
   it('should return HTML file: index', function() {
     return chai.request(app)
       .get('')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.html;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+
+});
+
+describe('dashboard.html', function() {
+
+  it('should return HTML file: dashboard', function() {
+    return chai.request(app)
+      .get('/dashboard')
       .then(function(res) {
         res.should.have.status(200);
         res.should.be.html;
